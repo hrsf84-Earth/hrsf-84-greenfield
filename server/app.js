1ba14531c13fffbec3c4cfe8bc3a0451f8046e32
Jeff <jeffreyRstocker@gmail.com>
2017-11-14 12:04:15 -0800

    Adjusted Hello Earth Message

--- server/app.js
+++ server/app.js
@@ -5,5 +5,5 @@ var port = process.env.PORT || 8080;
 app.listen(port, () => {console.log ('listening to port: ', port)})
 
 app.use(function (req, res) {
-  res.status(200).send('Hello Earth')
+  res.status(200).send('Hello Earth - Welcome To the Web')
 })
