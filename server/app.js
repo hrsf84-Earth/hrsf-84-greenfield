c903acb655dfbb3e3ef261a50a16d9c93861fc6a
Jeff <jeffreyRstocker@gmail.com>
2017-11-14 11:55:05 -0800

    Updated npm files

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 var express = require ('express')
 var app = express();
 
-var port = 8080;
+var port = process.env.PORT || 8080;
 app.listen(port, () => {console.log ('listening to port: ', port)})
 
 app.use(function (req, res) {
