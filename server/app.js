d49c1f187361dd197ec11c4bf6e757b4c04e6610
Jeff <jeffreyRstocker@gmail.com>
2017-11-16 10:34:10 -0800

    Commited 4th Heroku request

--- server/app.js
+++ server/app.js
@@ -9,14 +9,6 @@ app.use(function (req, res) {
 })
 
 app.get('/', function(req, res) {
-<<<<<<< 4e7ccd04bba99e88870bf4e8e1fd0c5937605b66
   res.status(201).send('received the initial get from the Client, the photo from API will go here')
 })
-=======
-	res.status(201).send('received the initial get from the Client, the photo from API will go here')
-})
 
-//comment test for Heroku, 
-// more testing
-//3rd test
->>>>>>> Commented for 3rd try for testing
