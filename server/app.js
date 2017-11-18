27ca05d907c8f76ac76a5adc3ced5589963abdc0
Carter Chavez <Carter.chavez17@gmail.com>
2017-11-18 14:27:36 -0800

    initial get request to server (#5)

--- server/app.js
+++ server/app.js
@@ -8,6 +8,10 @@ app.use(function (req, res) {
   res.status(200).send('Hello Earth - Welcome To the Web')
 })
 
+app.get('/', function(req, res) {
+	res.status(201).send('received the initial get from the Client, the photo from API will go here')
+})
+
 //comment test for Heroku, 
 // more testing
 //3rd test
