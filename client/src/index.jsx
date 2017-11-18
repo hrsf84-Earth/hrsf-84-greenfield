390c64ee65b3e87a34c9abdc3b37da0ec8b1134e
Janet Lee <5065781+janetlee@users.noreply.github.com>
2017-11-18 14:22:20 -0800

    Add skeleton client directories with index.html file, index.jsx, modify .gitignore to include bundle.js but not the entire /dist dir (#4)

---
+++ client/src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
