3b65066759d5e690b5f0d2a67b05ba23634d0a11
tgrahams101 <tanyansi@gmail.com>
2017-11-18 18:24:41 -0800

    finished unsplash api server-side call. there's a route that when hit will call the unsplash api with the query parameters and return json of the relevant photo data (#16)

--- server/app.js
+++ server/app.js
@@ -1,15 +1,29 @@
 var express = require ('express')
 var app = express();
+var unsplash = require('../helpers/unsplash.js');
 
 var port = process.env.PORT || 8080;
 app.listen(port, () => {console.log ('listening to port: ', port)})
 
+// app.use(function (req, res) {
+//   res.status(200).send('Hello Earth - Welcome To the Web')
+// })
 app.use(express.static('./client/dist'))
-
-app.use(function (req, res) {
-  res.status(200).send('Hello Earth - Welcome To the Web')
-})
 
 app.get('/', function(req, res) {
   res.status(201).send('received the initial get from the Client, the photo from API will go here')
 })
+
+app.get('/photos', function(req, res){
+
+  console.log('REQ QUERY', req.query);
+  unsplash.getPhotos(req.query, function(err, data){
+    if(err){
+      res.status(404).send('ERROR RETRIEVING PHOTOS');
+    }
+    console.log(data);
+    res.send(data);
+
+  });
+
+});
