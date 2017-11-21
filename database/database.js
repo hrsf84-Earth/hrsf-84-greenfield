ecfb8aa037086e91899c7e24e40e2d91082ba7ce
Jeff <jeffreyRstocker@gmail.com>
2017-11-20 18:51:03 -0800

    init refactor

--- database/database.js
+++ database/database.js
@@ -32,3 +32,40 @@ con.connect(function(err) {
 
 
 module.exports.con = con;
+
+
+////////////// sessions ///////////////////////
+
+var MySQLStore = require('express-mysql-session')(session);
+var crypto = require('../helpers/authentication.js');
+
+// Creates session options for express-session with MySQLStore
+var options = {
+  host: JAWSDB_URL,
+  port: mySQL_port,
+  user: mySQL_username,
+  password: mySQL_password,
+  database: mySQL_DBName,
+  expiration: 600000,
+  // Whether or not to create the sessions database table, if one does not already exist:
+  createDatabaseTable: false
+};
+
+// Need to uncomment this for connecting to mySQL
+// var connection = mysql.createConnection(options); // or mysql.createPool(options);
+var sessionStore = new MySQLStore(options);
+
+// Creates the session information and makes function for issuing a cookie.
+session({
+  key: 'impulse_cookie_ID',
+  secret: 'impulse_love_photos',
+  store: sessionStore,
+  resave: false,
+  saveUninitialized: false
+})
+
+// Access the session as req.session
+module.exports.createCookie = ((req, res, next) => {
+  res.cookie('impulse_cookie_ID', crypto.createRandom32String())
+  next();
+})
