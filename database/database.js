238f8680ed7cc80356b311d51418f14ff5138b26
JeffreyStocker <31262527+JeffreyStocker@users.noreply.github.com>
2017-11-20 19:29:30 -0800

    Refactor/move session to database (#34)
    * init refactor
    * Moved require session to database
    * Commented out duplicate variable declaration
    * Removed extra information
    * move chunks of code around for better reading

--- database/database.js
+++ database/database.js
@@ -1,4 +1,8 @@
 var mysql = require('mysql');
+var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
+var crypto = require('../helpers/authentication.js');
+
 try { 
   //try to find config file, if doesn't exist then assume that 
   //server is live and will retrieve variables from enviromental variables on server
@@ -7,14 +11,14 @@ try {
 } finally {
   var mySQL_username = process.env.mySQL_username ||  config.mySQL_username ;
   var mySQL_password = process.env.mySQL_password || config.mySQL_password;
+  var mySQL_DBName = process.env.JAWSDB_URL || config.JAWSDB_URL;
   var mySQL_port = process.env.mySQL_port || config.mySQL_port;
-  // var JAWSDB_URL = process.env.JAWSDB_URL || config.JAWSDB_URL;
+  var JAWSDB_URL = process.env.JAWSDB_URL || config.JAWSDB_URL;
 }
 
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
-
 var con = mysql.createConnection({
   host: 'kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   user: mySQL_username,
@@ -30,5 +34,37 @@ con.connect(function(err) {
   }
 });
 
+////////////// sessions ///////////////////////
+
+// Creates session options for express-session with MySQLStore
+var mySQLStoreOptions = {
+  host: JAWSDB_URL,
+  port: mySQL_port,
+  user: mySQL_username,
+  password: mySQL_password,
+  database: mySQL_DBName,
+  expiration: 600000,
+  // Whether or not to create the sessions database table, if one does not already exist:
+  createDatabaseTable: false
+};
+
+// Need to uncomment this for connecting to mySQL
+// var connection = mysql.createConnection(options); // or mysql.createPool(options); /js: this one does not work, one above does
+var sessionStore = new MySQLStore(mySQLStoreOptions);
+
+// Creates the session information and makes function for issuing a cookie.
+session({
+  key: 'impulse_cookie_ID',
+  secret: 'impulse_love_photos',
+  store: sessionStore,
+  resave: false,
+  saveUninitialized: false
+})
+
+// Access the session as req.session
+module.exports.createCookie = ((req, res, next) => {
+  res.cookie('impulse_cookie_ID', crypto.createRandom32String())
+  next();
+})
 
 module.exports.con = con;
--- server/app.js
+++ server/app.js
@@ -1,64 +1,24 @@
 var bodyParser = require('body-parser');
-var config = require('../config.js');
-var db = require ('../database/database.js');
 var express = require ('express');
-var session = require('express-session');
-var MySQLStore = require('express-mysql-session')(session);
+
+var db = require ('../database/database.js');
 var unsplash = require('../helpers/unsplash.js');
-var crypto = require('../helpers/authentication.js');
 
 var app = express();
 
 var port = process.env.PORT || 8080;
-app.listen(port, () => {console.log ('listening to port: ', port)});
 
-// Sets variables for use with cookie persistence
-var mySQL_username = process.env.mySQL_username ||  config.mySQL_username ;
-var mySQL_password = process.env.mySQL_password || config.mySQL_password;
-var mySQL_port = process.env.mySQL_port || config.mySQL_port;
-var mySQL_DBName = process.env.mySQL_DBName || config.mySQL_DBName;
-var JAWSDB_URL = process.env.JAWSDB_URL || config.JAWSDB_URL;
-
-// Creates session options for express-session with MySQLStore
-var options = {
-  host: JAWSDB_URL,
-  port: mySQL_port,
-  user: mySQL_username,
-  password: mySQL_password,
-  database: mySQL_DBName,
-  expiration: 600000,
-  // Whether or not to create the sessions database table, if one does not already exist:
-  createDatabaseTable: false
-};
-
-// Need to uncomment this for connecting to mySQL
-// var connection = mysql.createConnection(options); // or mysql.createPool(options);
-var sessionStore = new MySQLStore(options);
-
-// Creates the session information and makes function for issuing a cookie.
-app.use(session({
-  key: 'impulse_cookie_ID',
-  secret: 'impulse_love_photos',
-  store: sessionStore,
-  resave: false,
-  saveUninitialized: false
-}));
-
-// Access the session as req.session
-app.use(function(req, res, next) {
-    res.cookie('impulse_cookie_ID', crypto.createRandom32String())
-    next();
-});
+app.use(db.createCookie)
 
 app.use(express.static('./client/dist'));
 
 app.get('/photos', function(req, res){
-
+  
   console.log('REQ QUERY', req.query);
-
+  
   var term = req.query.query || 'tigers';
   console.log('THE TERM,', term.query)
-
+  
   unsplash.getPhotos(term, function(err, data){
     if(err){
       res.status(404).send('ERROR RETRIEVING PHOTOS' + err);
@@ -69,7 +29,7 @@ app.get('/photos', function(req, res){
   });
 });
 
- app.get('/search', function(req, res) {
+app.get('/search', function(req, res) {
   //need to update req so it posts the right data in getPhotos
   unsplash.getPhotos(req, function(err, data){
     if(err){
@@ -80,3 +40,6 @@ app.get('/photos', function(req, res){
     }
   });
 });
+
+
+app.listen(port, () => {console.log ('listening to port: ', port)});
