9c07eaf6b9920bcc26622e3615a0ebfebffe398e
Janet Lee <5065781+janetlee@users.noreply.github.com>
2017-11-21 11:07:34 -0800

    Add feature to retrieve more pictures (#36)

--- helpers/unsplash.js
+++ helpers/unsplash.js
@@ -12,19 +12,18 @@ try {
 }
 
 
-const getPhotos = (input, callback) => {
+const getPhotos = (input, page, callback) => {
   //PARAMETERS FOR UNSPLASH API
 // query:	Search terms.
 // page:	Page number to retrieve. (Optional; default: 1)
 // per_page:	Number of items per page. (Optional; default: 10)
 // collections:	Collection ID(‘s) to narrow search. If multiple, comma-separated.
   axios.get('https://api.unsplash.com/search/photos', {
-    params: {query: input, per_page: "30"}, headers: {'Authorization': 'Client-ID 9c7f8bd9c717a9fb38a8cf1c8f5bfa39372aa246790420c14659c31c4d1261fe'}
+    params: {query: input, page: '"' + page + '"', per_page: "30"}, headers: {'Authorization': 'Client-ID 9c7f8bd9c717a9fb38a8cf1c8f5bfa39372aa246790420c14659c31c4d1261fe'}
   })
   .then(photosFromAPI => {
     console.log('THE LENGTH BRUH', photosFromAPI.data.results.length)
     callback(null, photosFromAPI.data.results);
-
   })
   .catch(apiError => {
     callback(apiError, null);
