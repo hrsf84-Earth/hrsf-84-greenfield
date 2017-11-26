a822214ca32201e08753f8faff1e22c0b0e67f08
Janet Lee <janetjhu96@gmail.com>
2017-11-26 15:08:56 -0800

    Write client side tests for div and button counts on initial layout

--- spec/Client/test.js
+++ spec/Client/test.js
@@ -3,7 +3,7 @@ import Enzyme from 'enzyme';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import sinon from 'sinon';
-import { expect } from 'chai';
+import { expect, assert, should } from 'chai';
 import { render, mount, shallow } from 'enzyme';
 import App from '../../client/src/index.jsx';
 import Search from '../../client/src/components/search';
@@ -13,41 +13,68 @@ import Blanket from 'blanket';
 
 configure({ adapter: new Adapter() });
 
-describe('App', () => {
+describe('AppView', function() {
+  var view;
 
-  it('should properly run tests', () => {
-    expect(1).equal(1);
+  beforeEach(function() {
+    sinon.spy(App.prototype, 'render');
+    sinon.spy(Carousel.prototype, 'render');
+    sinon.spy(Search.prototype, 'render');
+    sinon.spy(Login.prototype, 'render');
+
+    view = new App();
+    view.src.reset([]);
   });
 
-  it('should be an Function', function() {
-    expect(App).to.be.an('Function');
+  afterEach(function() {
+    App.prototype.render.restore();
+    Carousel.prototype.render.restore();
+    Search.prototype.render.restore();
+    Login.prototype.render.restore();
   });
 
-  it('should be a stateful class component', function() {
-    expect(React.Component.isPrototypeOf(App)).to.be.true;
+  it('should render itself', function() {
+    expect(App.prototype.render).to.have.callCount(1);
+  });
+
+  it('should render a Carousel view', function() {
+    expect(Carousel.prototype.render).to.have.callCount(1);
+  });
+
+  it('should render a Search view', function() {
+    expect(Search.prototype.render).to.have.callCount(1);
+  });
+
+  it('should render a Login view', function() {
+    expect(Login.prototype.render).to.have.callCount(1);
   });
 });
 
+<<<<<<< HEAD
 describe("AppScreen", () => {
+=======
+
+describe("App", () => {
+>>>>>>> Write client side tests for div and button counts on initial layout
   let props;
   let mountedAppScreen;
   const appScreen = () => {
     if (!mountedAppScreen) {
-      mountedAppScreen = mount(<App />);
+      mountedAppScreen = mount(<App {...props} />);
     }
     return mountedAppScreen;
   }
 
   beforeEach(() => {
-    props = {
-      currentPhotoIndex: undefined,
-      searchTerm: undefined,
-      searchPagination: undefined,
-    };
+    // props = {
+    //   currentPhotoIndex: undefined,
+    //   searchTerm: undefined,
+    //   searchPagination: undefined,
+    // };
     mountedAppScreen = undefined;
   });
-});
 
+<<<<<<< HEAD
 describe('App', () => {
 
 sinon.spy(App.prototype, 'componentWillMount');
@@ -134,23 +161,60 @@ describe('Login', () => {
   it('renders 5 <div> tags', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find('div')).to.have.length(5);
+=======
+  xit('should properly run tests', () => {
+    expect(1).equal(1);
+>>>>>>> Write client side tests for div and button counts on initial layout
   });
 
-  it("contains everything else that gets rendered", () => {
-      const wrappingDiv = appScreen().find("div");
-      expect(wrappingDiv.children().first()).to.deep.equal(appScreen().children().first());
+  xit('should be an Function', function() {
+    expect(App).to.be.an('Function');
   });
 
-  xit('App.handlePhotoNavigationClick should exist', function() {
-    console.log('Try to console an APP property', App._this.handlePhotoNavigationClick.toString());
-    expect(App.handlePhotoNavigationClick).exist;
+  xit('should be a stateful class component', function() {
+    expect(React.Component.isPrototypeOf(App)).to.be.true;
   });
 
-  xit('simulates click events', () => {
-    const handlePhotoNavigationClick = sinon.spy();
-    // const wrapper = mount((<App handlePhotoNavigationClick={handlePhotoNavigationClick} />));
-    wrapper.find('button').simulate('click');
-    expect(handlePhotoNavigationClick).to.have.property('callCount', 1);
+  describe('<App />', () => {
+    sinon.spy(App.prototype, 'componentWillMount');
+
+    it('renders 5 <div> tags for major components on shallow', () => {
+      const wrapper = shallow(<App />);
+      expect(wrapper.find('div')).to.have.length(5);
+    });
+
+    it("contains everything else on mount with strict equal", () => {
+      const wrapper = appScreen();
+      expect(wrapper.find('div').children()).to.have.length(14);
+    });
+
+
+    it("contains everything else on mount with deep equal", () => {
+        const wrapper = appScreen().find("div");
+        expect(wrapper.children()).to.deep.equal(wrapper.find('div').children());
+    });
+
+    it("contains everything else on mount with deep equal", () => {
+        const wrapper = appScreen().find("button");
+        expect(wrapper.children()).to.deep.equal(wrapper.find('button').children());
+    });
+
+    it('renders 5 buttons tags for major components on mount', () => {
+      const wrapper = appScreen();
+      expect(wrapper.find('button')).to.have.length(5);
+    });
+    // it('App.handlePhotoNavigationClick should exist', function() {
+    //   const handlePhotoClick = appScreen().find(handlePhotoNavigationClick);
+    //   expect(handlePhotoClick.props().children).to.be.true;
+      // assert.exists(wrapper.handlePhotoNavigationClick);
+    // });
+
+    xit('simulates click events', () => {
+      const handlePhotoNavigationClick = sinon.spy();
+      // const wrapper = mount((<App handlePhotoNavigationClick={handlePhotoNavigationClick} />));
+      wrapper.find('button').simulate('click');
+      expect(handlePhotoNavigationClick).to.have.property('callCount', 1);
+    });
   });
 });
 
@@ -186,23 +250,23 @@ xdescribe('Carousel', () => {
 });
 
 
-xdescribe('Login', () => {
-  it('should properly run tests', () => {
+describe('Login', () => {
+  xit('should properly run tests', () => {
     expect(1).equal(1);
   });
 
-  it('should be an Function', () => {
+  xit('should be an Function', () => {
     expect(Login).to.be.an('Function');
   });
 
-  it('should be a stateful class component', function() {
+  xit('should be a stateful class component', function() {
     expect(React.Component.isPrototypeOf(Login)).to.be.true;
   });
 
-  xit('Login.submitInformation should exist', function() {
+  it('Login.submitInformation should exist', function() {
     console.log(Login.toString());
-    console.log('Try to console an Login property', Login.submitInformation .toString());
-    expect(Login.submitInformation ).exist;
+    console.log('Try to console an Login property', Login.submitInformation.toString());
+    expect(Login.submitInformation).exist;
   });
 
   xit('Login.submitInformation should exist', function() {
