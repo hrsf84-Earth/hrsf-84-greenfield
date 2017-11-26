62f38b6f3ee4a33bd7bf6ae69e08b95851ef65fd
Janet Lee <janetjhu96@gmail.com>
2017-11-25 17:27:57 -0800

    Fix git merge conflicts

--- spec/Client/test.js
+++ spec/Client/test.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Enzyme from 'enzyme';
+<<<<<<< HEAD
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import sinon from 'sinon';
+=======
+import Adapter from 'enzyme-adapter-react-16';
+>>>>>>> Fix merge conflict  and add the test.js wrapper for JDOM
 import { expect } from 'chai';
 import { render, mount, shallow } from 'enzyme';
 import App from '../../client/src/index.jsx';
@@ -35,6 +39,74 @@ xdescribe('<App />', () => {
   });
 });
 
+describe('App', () => {
+
+  it('should properly run tests', () => {
+    expect(1).equal(1);
+  });
+
+  it('should be an Function', function() {
+    expect(App).to.be.an('Function');
+  });
+
+  it('should be a stateful class component', function() {
+    expect(React.Component.isPrototypeOf(App)).to.be.true;
+  });
+
+  it('App.handlePhotoNavigationClick should exist', function() {
+    // console.log(App.toString());
+    console.log('Try to console an APP property', App._this.handlePhotoNavigationClick.toString());
+    expect(App.handlePhotoNavigationClick).exist;
+  });
+});
+
+
+describe('Search', () => {
+  it('should properly run tests', () => {
+    expect(1).equal(1);
+  });
+
+  it('should be a Function', () => {
+    expect(Search).to.be.an('Function');
+  });
+
+  it('should be a stateless class component', function() {
+    expect(React.Component.isPrototypeOf(Search)).to.be.false;
+  });
+
+});
+
+
+describe('Carousel', () => {
+  it('should properly run tests', () => {
+    expect(1).equal(1);
+  });
+
+  it('should be a function', () => {
+    expect(Carousel).to.be.an('function');
+  });
+
+  it('should be a stateless class component', function() {
+    expect(React.Component.isPrototypeOf(Carousel)).to.be.false;
+  });
+
+});
+
+
+describe('Login', () => {
+  it('should properly run tests', () => {
+    expect(1).equal(1);
+  });
+
+  it('should be an Function', () => {
+    expect(Login).to.be.an('Function');
+  });
+
+  it('should be a stateless class component', function() {
+    expect(React.Component.isPrototypeOf(Login)).to.be.false;
+  });
+
+});
 
   xit('renders one <App /> component', () => {
     const wrapper = shallow(<App />);
