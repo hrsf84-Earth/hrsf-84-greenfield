d249ad85f2ed473e493bcd0ba187f6ee39d178a1
Janet Lee <janetjhu96@gmail.com>
2017-11-26 20:50:53 -0800

    Realign elements to be responsive and closer match to wireframe

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -28,6 +28,10 @@ export default class App extends React.Component {
     this.saveUserName = this.saveUserName.bind(this);
   }
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> Realign elements to be responsive and closer match to wireframe
   componentWillMount() {
     var context = this;
 
